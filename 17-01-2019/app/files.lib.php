edfc05158f71601429ad3ad44a14a8da3107fcf3
girafffee <sanko200065@gmail.com>
2019-01-17 20:39:48 +0200

    Function Parse

---
+++ 17-01-2019/app/files.lib.php
@@ -0,0 +1,48 @@
+<?php
+
+function parserGuest(){
+	$ret = "";
+	$fileBase = "/17-01-19/storage/files"; //базовая папка
+	$fileName = "guest.txt"; // имя файла
+	$filePath =__DIR__."/..".$fileBase."/".$fileName;
+	//$ret .= __FILE__;
+
+	//$fromFile = file($filePath);
+	//$ret.= PP($fromFile);
+
+	$handle = @fopen($filePath, "r");
+	if ($handle) {// Если файл открылся, то ..
+    	while (($buffer = fgets($handle)) !== false) { //читаем по строкам
+        $ret.= $buffer;
+        //проверка на телефон или почту
+        //smsGate || mailGate
+    }
+    if (!feof($handle)) {
+        $ret .= "Ошибка: fgets() неожиданно потерпел неудачу\n";
+    }
+    fclose($handle);
+}
+	return $ret;
+}
+
+function parsePhoto(){
+	$ret ="";
+	$fileBase ="/17-01-19/storage/files"; // базовая папка
+	$filePath= __DIR__ . "/.." . $fileBase . "/";
+
+	$arr = scandir($filePath);
+	$ret .= PP($arr);
+
+	$ret .= '<ul>';
+
+	for($i = 0; $i < sizeof($arr); $i++){
+		if(exif_imagetype($filePath.$arr[$i])){ // проверяем является ли файл картинкой
+		$ret .= '<li><img src="'.$filesBase.'/'.$arr[$i].'"width="50">'; // выводим картинку
+		$ret .= date("F d Y H:i:s.", fileatime($filesPath. $arr[$i]));
+		
+		$ret .= '</li>'
+		}
+	}
+	$ret .= '</ul>'
+	return $ret;
+}
---
+++ 17-01-2019/public/index.php
@@ -0,0 +1,22 @@
+<?php
+$startFrameworkTime = "";
+include ("../app/templates.lib.php");
+include ("../app/vardump.lib.php");
+include ("../app/files.lib.php");
+
+
+$responce ['header']['sitename'] = "Hello World";
+//$responce ['head']['title'] = "My First tpl";
+$responce ['head']['css'] = "";
+$responce ['footer']['copy'] = "&copy IT Step";
+$responce ['content'] = '<h1 class="w-100"> Content </h1>';
+
+
+
+//$responce['content'] = parserGuest();
+$responce['content'] = parserPhoto();
+
+//--------------------------------
+buildPage($responce);
+
+
